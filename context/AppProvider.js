4d0926c063cc7af299e6fc2f7fa425f390830619
German O. Macchia <germanmacchia@gmail.com>
2022-09-24 10:44:31 -0300

    typo

--- context/AppProvider.js
+++ context/AppProvider.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { AppContext } from "./AppContext";
 
 export const AppProvider = ({ children }) => {
-  const initialSSR = 0
+  const initialValue = 0
   const [reload, setReload] = useState(false);
-  const [quantity, setQuantity] = useState(initialSSR);
+  const [quantity, setQuantity] = useState(initialValue);
 
   const initialData = {
     avatarQuantity: {
